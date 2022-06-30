d0586ba0e1b18865fe3bfe6ebe95287a86c95224
Arunkutty798 <arunkutty798@gmail.com>
2022-06-30 18:40:45 +0530

    choke:works

--- src/section/Works/work.tsx
+++ src/section/Works/work.tsx
@@ -4,12 +4,9 @@ const Work = () => {
   return (
     <div className="work">
       <h1 data-aos='fade-up'>HOW IT WORKS?</h1>
-      <iframe
-       className="video"
-        src="https://www.youtube.com/embed/tgbNymZ7vqY"
-        title="youtube"
-        data-aos='fade-up'
-      ></iframe>
+      <div>
+        <h1>Updating Soon. . .</h1>
+      </div>
     </div>
   );
 };
