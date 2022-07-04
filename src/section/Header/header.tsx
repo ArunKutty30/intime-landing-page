8a96d15a5c076df02fb0b0f4823a2d24a9628eef
Arunkutty798 <arunkutty798@gmail.com>
2022-07-04 18:04:04 +0530

    choke:header.tsx

--- src/section/Header/header.tsx
+++ src/section/Header/header.tsx
@@ -8,28 +8,33 @@ const Header = () => {
   const [value, setValue] = useState(false);
 
   return (
-    <div className="header">
-      <div>
-        <h1>INTIME</h1>
-      </div>
-      <div className="menu">
-        <a href="/">Home</a>
-        <a href="#about">About</a>
-        <a href="#roadmap">Roadmap</a>
-        <a href="#team">Team</a>
-        <a href="#team">Whitepaper</a>
-      </div>
-      <a href="https://marketplace.intimenft.app" target="blank">
-        <Button style={{ fontFamily: "Grind" }} className="btn">
-          Launch App
-        </Button>
-      </a>
+    <>
+      <div className="header">
+        <div>
+          <h1>INTIME</h1>
+        </div>
+        <div className="menu">
+          <a href="/">Home</a>
+          <a href="#about">About</a>
+          <a href="#roadmap">Roadmap</a>
+          <a href="#team">Team</a>
+          <a href="#team">Whitepaper</a>
+        </div>
+        <a href="https://marketplace.intimenft.app" target="blank">
+          <Button style={{ fontFamily: "Grind" }} className="btn">
+            Launch App
+          </Button>
+        </a>
 
-      <div className="nav_menu" onClick={() => setValue(!value)}>
-        <img src={menu} alt="menu" width="30" height="30"></img>
+        <div className="nav_menu" onClick={() => setValue(!value)}>
+          <img src={menu} alt="menu" width="30" height="30"></img>
+        </div>
+        {value && <Sidebar setValue={setValue} />}
       </div>
-      {value && <Sidebar setValue={setValue} />}
-    </div>
+      <div className="merchandise">
+        Check out our <a href="https://store.intimenft.app">Merchandise</a>
+      </div>
+    </>
   );
 };
 
